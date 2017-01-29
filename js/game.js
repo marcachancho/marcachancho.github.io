e67ec47cad71d3a6620c631c04e16d4cd756aefe
ricdodds <ric.dodds@gmail.com>
2017-01-28 19:59:09 -0500

    added some progress

--- js/game.js
+++ js/game.js
@@ -1,30 +1,56 @@
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload: preload, create: create });
 
 function preload () {
-	game.load.text('dictionary', 'static/coa.txt');
-	game.load.image('logo', 'phaser.png');
+	game.load.json('dictionary', 'static/coa.json');
 }
+
 
 function create () {
+	game.stage.backgroundColor = '#D9D9D9'
+	
+	var dictionary = shuffle(game.cache.getJSON('dictionary'));
+	console.log(dictionary.slice(0,5));
 
-	var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-	logo.anchor.setTo(0.5, 0.5);
-
-	// resizeGame();
 }
 
-function resizeGame(){
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function render() {
+
+	var textGroup = game.add.group();
+
+	for (var i = 0; i < 300; i++) {
+		textGroup.add(game.make.text(32, i * 20, text[i]))
+		console.log(text[i])
+	}
+
+}
+
+function resizeGame() {
 	var height = window.innerHeight;
 	var width = window.innerWidth;
-	 
+
 	game.width = width;
 	game.height = height;
-	// game.stage.bounds.width = width;
-	// game.stage.bounds.height = height;
-	 
+
 	if (game.renderType === 1) {
-	    game.renderer.resize(width, height);
-	    Phaser.Canvas.setSmoothingEnabled(game.context, false);
+		game.renderer.resize(width, height);
+		Phaser.Canvas.setSmoothingEnabled(game.context, false);
 	}
 }
 
---
+++ static/to_json.py
@@ -0,0 +1,17 @@
+import json
+
+f = open('coa.txt', 'r')
+
+dictonary = []
+for line in f:
+    word_value = line.split(' - ')
+    word = word_value[0]
+    value = word_value[1].strip()
+
+    dictonary.append({
+        'word': word,
+        'value': value,
+    })
+
+with open('coa.json', 'w') as outfile:
+    json.dump(dictonary, outfile, ensure_ascii=False)
